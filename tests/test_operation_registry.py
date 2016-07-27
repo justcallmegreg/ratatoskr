750da6a05b6d8d89c347877f605fa246a3597640
Gergő Nagy <grigori.grant@gmail.com>
2016-07-27 13:56:58 +0200

    tests: basic test cases
    * `protectron` testcases
    * `operation_wrapper` testcases
    * `MANIFESTS.md` change to wrap tests
    * `requirements-dev.txt`: test depenendencies
    
    Signed-off-by: Gergő Nagy <grigori.grant@gmail.com>

---
+++ tests/test_operation_registry.py
@@ -0,0 +1,68 @@
+import pytest
+
+from ratatoskr import register_operation, dispatch_event
+import ratatoskr
+
+def test_operation_dispatch():
+
+    @register_operation()
+    def foo():
+        return 42
+
+    @register_operation()
+    def bar():
+        return 24
+
+    event = {
+        'operation': 'foo',
+        'args': {}
+    }
+
+    assert dispatch_event(event) == 42
+
+
+def test_operation_dispatch_with_custom_operation_wrapper():
+
+    @register_operation(ratatoskr.base_wrappers.LocalOperation())
+    def foo2():
+        return 42
+
+    @register_operation(ratatoskr.base_wrappers.LocalOperation())
+    def bar2():
+        return 24
+
+    event = {
+        'operation': 'foo2',
+        'args': {}
+    }
+
+    assert dispatch_event(event) == 42
+
+
+def test_operation_dispatch_with_args():
+
+    @register_operation()
+    def foo3(value):
+        return value
+
+    event = {
+        'operation': 'foo3',
+        'args': {'value': 69}
+    }
+
+    assert dispatch_event(event) == 69
+
+
+def test_decorator_without_parantheses():
+
+    @register_operation
+    def foo3(value):
+        return value
+
+    event = {
+        'operation': 'foo3',
+        'args': {'value': 69}
+    }
+
+    assert dispatch_event(event) == 69
+
---
+++ tests/test_protectron.py
@@ -0,0 +1,112 @@
+import pytest
+
+from ratatoskr import protectron
+from voluptuous import Schema, Invalid
+
+
+def test_protectron_empty_input_schema():
+
+    with pytest.raises(TypeError):
+
+        @protectron()
+        def foo(a):
+            return a
+
+
+def test_protectron_simple_input_schema_match():
+
+    @protectron(Schema({'a': int}))
+    def foo(a):
+        return a
+
+    assert foo(42) == 42
+
+
+def test_protectron_simple_input_schema_unmatch():
+
+    @protectron(Schema({'a': int}))
+    def foo(a):
+        return a
+
+    with pytest.raises(Invalid):
+        foo('42')
+
+
+def test_protectron_kwargs_input_schema_match():
+
+    @protectron(Schema({'a': int, 'b': int}))
+    def foo(a, b):
+        return a
+
+    assert foo(a=42, b=42) == 42
+
+
+def test_protectron_kwargs_input_schema_unmatch():
+
+    @protectron(Schema({'a': int, 'b': int}))
+    def foo(a, b):
+        return a
+
+    with pytest.raises(Invalid):
+        foo(a='42', b=42)
+
+
+def test_protectron_empty_output_schema():
+
+    with pytest.raises(TypeError):
+
+        @protectron()
+        def foo(a):
+            return a
+
+
+def test_protectron_simple_output_schema_match():
+
+    @protectron(Schema({'a': int}), Schema(int))
+    def foo(a):
+        return a
+
+    assert foo(42) == 42
+
+
+def test_protectron_simple_output_schema_unmatch():
+
+    @protectron(Schema({'a': int}), Schema(int))
+    def foo(a):
+        return a
+
+    with pytest.raises(Invalid):
+        foo('42')
+
+
+# TODO: refactor testcase to check against tuple of integers
+def test_protectron_multiple_output_schema_match():
+
+    @protectron(Schema({'a': int, 'b': int}), Schema(tuple))
+    def foo(a, b):
+        return a, b
+
+    assert foo(a=42, b=42) == (42, 42)
+
+
+# TODO: refactor testcase to check against tuple of integers
+def test_protectron_multiple_output_schema_unmatch():
+
+    @protectron(Schema({'a': int, 'b': int}), Schema(int))
+    def foo(a, b):
+        return a, b
+
+    with pytest.raises(Invalid):
+        foo(a='42', b=42)
+
+
+def test_protectron_kwargs_output_schema_match():
+
+    input_schema = Schema({'a': int, 'b': int})
+    output_schema = Schema(tuple)
+
+    @protectron(input_schema, output_schema)
+    def foo(a, b):
+        return a, b
+
+    foo(42, 42) == (42, 42)

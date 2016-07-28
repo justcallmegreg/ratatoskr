d469ba4eb01244b2a8d20591c4e3f30f7867ffbc
Gergő Nagy <grigori.grant@gmail.com>
2016-07-28 14:04:31 +0200

    logging: operation_registry
    Main stages of the lifetime of an operation is logged.
    
    Signed-off-by: Gergő Nagy <grigori.grant@gmail.com>

--- ratatoskr/operation_registry.py
+++ ratatoskr/operation_registry.py
@@ -1,6 +1,7 @@
 from protectron import protectron
 from schema import ValidOperationRegistryEventSchema
 from operation_wrappers.base_wrappers import OperationWrapper
+from internal_logger import LOG
 
 
 class InvalidOperationWrapperError(Exception):
@@ -14,15 +15,24 @@ class OperationRegistry:
     @classmethod
     def register_operation(cls, operation_wrapper):
         if not issubclass(operation_wrapper.__class__, OperationWrapper):
+
+            LOG.error('operation_wrapper [%s] is not a sublass of OperationWrapper',
+                      operation_wrapper.__class__)
+
             raise InvalidOperationWrapperError(
                 'operation_wrapper must be a subclass of OperationWrapper'
             )
+
         operation_name = operation_wrapper.get_wrapped_operation_name()
         OperationRegistry.registry[operation_name] = operation_wrapper
+
+        LOG.info('operation [%s] registered, operation_wrapper is [%s]',
+                 operation_name, operation_wrapper.__class__)
 
     @classmethod
     def deregister_operation(cls, operation_name):
         del OperationRegistry.registry[operation_name]
+        LOG.info('operation [%s] deregistered', operation_name)
 
     @classmethod
     def list_operations(cls):
@@ -38,4 +48,8 @@ class OperationRegistry:
         operation_name = event['operation']
         arguments = event['args']
         operation_wrapper = OperationRegistry.registry[operation_name]
+        LOG.debug('event [%s] is dispatched to operation [%s]',
+                  event, operation_name)
+        LOG.info('operation [%s] is called',
+                 operation_name)
         return operation_wrapper.call(**arguments)

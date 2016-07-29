edad8c83c3c3c6da0355efc06b622db1498e1a04
Gergő Nagy <grigori.grant@gmail.com>
2016-07-29 13:23:47 +0200

    comments: aws_lambda
    Signed-off-by: Gergő Nagy <grigori.grant@gmail.com>

--- ratatoskr/operation_wrappers/aws_lambda.py
+++ ratatoskr/operation_wrappers/aws_lambda.py
@@ -10,12 +10,24 @@ from base_wrappers import RemoteOperation
 
 
 class AWSLambdaOperation(RemoteOperation):
+    """
+        OperationWrapper for calling other AWS Lambda functions
+        where the operation is implemented.
+    """
 
     def __init__(self, region, lambda_function):
+        """
+            @input `region` where the lambda function is present
+            @input `lambda_function` name of the lambda function to be called
+        """
         self.region = region
         self.lambda_function = lambda_function
 
     def call(self, *args, **kwargs):
+        """
+            Method that implements remote lambda call using `boto3`
+            and parses the response.
+        """
         cli = boto3.client('lambda', region_name=self.region)
         response = cli.invoke(
             FunctionName=self.lambda_function,

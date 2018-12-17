aa22ea36a634eb30dd5e330396233d55e86e1f70
Gergő Nagy <contact@gergonagy.com>
2018-12-17 11:27:05 +0100

    base_wrappers: custom operation name
    Introducing custom name for registered operations to make it easier for
    user to build up naming conventions in their OperationRegistry and
    help to avoid naming collisions.
    
    Signed-off-by: Gergő Nagy <contact@gergonagy.com>

--- ratatoskr/operation_wrappers/base_wrappers.py
+++ ratatoskr/operation_wrappers/base_wrappers.py
@@ -39,6 +39,15 @@ class LocalOperation(OperationWrapper):
     """
         Class to represent operations that must be executed on the host.
     """
+    def __init__(self, custom_name=str()):
+        self.custom_name = custom_name
+
+    def get_wrapped_operation_name(self):
+        """
+            Returns the name (id) of the operation that was loaded
+            into OperatioWrapper.
+        """
+        return self.custom_name if self.custom_name != "" else self.wrapped_operation.func_name
 
     def call(self, *args, **kwargs):
         return self.wrapped_operation(*args, **kwargs)
